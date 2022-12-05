167de018a86d7ff3a93f526ed64816e2ce051d17
Linus Unnebäck <linus@folkdatorn.se>
2022-12-05 13:10:32 +0200

    🐛 Add Ship struct to Day 5

--- src/day5.rs
+++ src/day5.rs
@@ -27,60 +27,77 @@ impl FromStr for Instruction {
     }
 }
 
+struct Ship {
+    stacks: Vec<Vec<char>>,
+}
+
+impl Ship {
+    fn result(&self) -> String {
+        let mut result = String::new();
+
+        for stack in &self.stacks {
+            result.push(*stack.last().unwrap());
+        }
+
+        result
+    }
+}
+
+impl FromStr for Ship {
+    type Err = ParseIntError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut boxes = Vec::<&str>::new();
+        let mut width: usize = 0;
+
+        for line in s.lines() {
+            if line.starts_with(" 1") {
+                assert!((line.len() + 1) % 4 == 0);
+                width = (line.len() + 1) / 4;
+            } else {
+                boxes.push(line);
+            }
+        }
+
+        boxes.reverse();
+
+        let mut stacks = vec![Vec::<char>::new(); width];
+
+        for line in boxes {
+            for (idx, c) in line.char_indices().filter(|(i, _)| i % 4 == 1) {
+                if c == ' ' {
+                    continue;
+                }
+
+                stacks[idx / 4].push(c)
+            }
+        }
+
+        Ok(Ship { stacks })
+    }
+}
+
 #[aoc(day5, part1)]
 pub fn part1(input: &str) -> Result<String, ParseIntError> {
-    let mut lines = input.lines();
+    let split_pos = input.find("\n\n").unwrap() + 2;
+    let (ship, instructions) = input.split_at(split_pos);
 
-    let mut boxes = Vec::<&str>::new();
-    let mut width: usize = 0;
+    let mut ship = ship.parse::<Ship>()?;
 
-    loop {
-        let line = lines.next().unwrap();
-
-        if line.starts_with(" 1") {
-            assert!((line.len() + 1) % 4 == 0);
-            width = (line.len() + 1) / 4;
-            continue;
-        }
-
-        if line.is_empty() {
-            break;
-        }
-
-        boxes.push(line);
-    }
-
-    boxes.reverse();
-
-    let mut stacks = vec![Vec::<char>::new(); width];
-
-    for line in boxes {
-        for (idx, c) in line.char_indices().filter(|(i, _)| i % 4 == 1) {
-            if c == ' ' {
-                continue;
-            }
-
-            stacks[idx / 4].push(c)
-        }
-    }
-
-    for instruction in lines {
+    for instruction in instructions.lines() {
         let instruction = instruction.parse::<Instruction>()?;
 
         for _ in 0..instruction.n {
-            let c = stacks[instruction.from].pop().unwrap();
+            let c = ship.stacks[instruction.from].pop().unwrap();
 
-            stacks[instruction.to].push(c);
+            ship.stacks[instruction.to].push(c);
         }
     }
 
-    let mut result = String::new();
-
-    for stack in stacks {
-        result.push(*stack.last().unwrap());
-    }
+    let result = ship.result();
 
     assert_ne!(result, "GGNPJBTTR");
+    assert_eq!(result, "QNNTGTPFN");
 
     Ok(result)
 }
