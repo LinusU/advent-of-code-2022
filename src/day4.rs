ac3b50c3bdad157741c924ad48e955ecc391842d
Linus Unnebäck <linus@folkdatorn.se>
2022-12-05 12:03:57 +0200

    🎉 Add solution to Day 4 - Part 2

--- src/day4.rs
+++ src/day4.rs
@@ -44,3 +44,27 @@ pub fn part1(input: &str) -> u64 {
         })
         .sum()
 }
+
+#[aoc(day4, part2)]
+pub fn part2(input: &str) -> u64 {
+    input
+        .split_whitespace()
+        .map(|line| {
+            let split_pos = line.chars().position(|c| c == ',').unwrap();
+            let (first, second) = line.split_at(split_pos);
+
+            let first = first.parse::<Range>().unwrap();
+            let second = second[1..].parse::<Range>().unwrap();
+
+            if first.start <= second.end && first.end >= second.start {
+                return 1;
+            }
+
+            if second.start <= first.end && second.end >= first.start {
+                return 1;
+            }
+
+            0
+        })
+        .sum()
+}
