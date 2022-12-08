c414c3b4db8f7a579463d93dc0e14671cca45206
Linus Unnebäck <linus@folkdatorn.se>
2022-12-08 08:54:40 +0200

    🐛 Add Forest struct to Day 8

--- src/day8.rs
+++ src/day8.rs
@@ -18,60 +18,103 @@ impl FromStr for Tree {
     }
 }
 
-#[aoc(day8, part1)]
-pub fn part1(input: &str) -> Result<u64, ParseIntError> {
-    let mut trees = input
-        .lines()
-        .map(|line| {
-            line.chars()
-                .map(|c| String::from(c).parse::<Tree>())
-                .collect::<Result<Vec<_>, _>>()
-        })
-        .collect::<Result<Vec<_>, _>>()?;
+struct Forest {
+    trees: Vec<Tree>,
+    width: usize,
+}
 
-    for line in trees.iter_mut() {
-        let mut h = -1;
-
-        for tree in line.iter_mut() {
-            if tree.height > h {
-                tree.visible = true;
-                h = tree.height;
-            }
-        }
-
-        let mut h = -1;
-
-        for tree in line.iter_mut().rev() {
-            if tree.height > h {
-                tree.visible = true;
-                h = tree.height;
-            }
-        }
+impl Forest {
+    fn height(&self) -> usize {
+        self.trees.len() / self.width
     }
 
-    let width = trees[0].len();
+    fn width(&self) -> usize {
+        self.width
+    }
+
+    fn iter(&self) -> impl DoubleEndedIterator<Item = &Tree> {
+        self.trees.iter()
+    }
+
+    fn iter_col_mut(&mut self, col: usize) -> impl DoubleEndedIterator<Item = &mut Tree> {
+        assert!(col < self.width());
+
+        self.trees.iter_mut().skip(col).step_by(self.width)
+    }
+
+    fn iter_row_mut(&mut self, row: usize) -> impl DoubleEndedIterator<Item = &mut Tree> {
+        assert!(row < self.height());
+
+        self.trees
+            .iter_mut()
+            .skip(row * self.width)
+            .take(self.width)
+    }
+}
+
+impl FromStr for Forest {
+    type Err = ParseIntError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let width = s.find('\n').unwrap_or(s.len());
+
+        let trees = s
+            .lines()
+            .flat_map(|line| line.chars().map(|c| String::from(c).parse::<Tree>()))
+            .collect::<Result<Vec<_>, _>>()?;
+
+        Ok(Forest { trees, width })
+    }
+}
+
+#[aoc(day8, part1)]
+pub fn part1(input: &str) -> Result<u64, ParseIntError> {
+    let mut forest = input.parse::<Forest>()?;
+
+    let width = forest.width();
+    let height = forest.height();
 
     for x in 0..width {
         let mut h = -1;
 
-        for tree in trees.iter_mut() {
-            if tree[x].height > h {
-                tree[x].visible = true;
-                h = tree[x].height;
+        for tree in forest.iter_col_mut(x) {
+            if tree.height > h {
+                tree.visible = true;
+                h = tree.height;
             }
         }
 
         let mut h = -1;
 
-        for tree in trees.iter_mut().rev() {
-            if tree[x].height > h {
-                tree[x].visible = true;
-                h = tree[x].height;
+        for tree in forest.iter_col_mut(x).rev() {
+            if tree.height > h {
+                tree.visible = true;
+                h = tree.height;
             }
         }
     }
 
-    Ok(trees.iter().flatten().map(|t| t.visible as u64).sum())
+    for y in 0..height {
+        let mut h = -1;
+
+        for tree in forest.iter_row_mut(y) {
+            if tree.height > h {
+                tree.visible = true;
+                h = tree.height;
+            }
+        }
+
+        let mut h = -1;
+
+        for tree in forest.iter_row_mut(y).rev() {
+            if tree.height > h {
+                tree.visible = true;
+                h = tree.height;
+            }
+        }
+    }
+
+    Ok(forest.iter().map(|t| t.visible as u64).sum())
 }
 
 #[cfg(test)]
