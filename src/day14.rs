175089372a5df6da8d727daceb23c9dde8f99687
Linus Unnebäck <linus@folkdatorn.se>
2022-12-14 09:35:52 +0100

    🎉 Add solution to Day 14 - Part 2

--- src/day14.rs
+++ src/day14.rs
@@ -37,59 +37,125 @@ impl FromStr for Coord {
     }
 }
 
-#[aoc(day14, part1)]
-pub fn part1(input: &str) -> Result<u32, ParseIntError> {
-    let mut blocked = HashSet::<Coord>::new();
-    let mut max_y = 0;
+struct Cave {
+    blocked: HashSet<Coord>,
+    max_y: u32,
+    sand_count: u32,
+}
 
-    for line in input.lines() {
-        let parts = line
-            .split(" -> ")
-            .map(Coord::from_str)
-            .collect::<Result<Vec<_>, _>>()?;
+impl Cave {
+    fn is_free(&self, pos: Coord) -> bool {
+        !self.blocked.contains(&pos)
+    }
 
-        max_y = max_y.max(parts.iter().map(|c| c.y).max().unwrap());
+    fn insert_sand(&mut self, pos: Coord) {
+        self.blocked.insert(pos);
+        self.sand_count += 1;
+    }
+}
 
-        for (a, b) in parts.iter().tuple_windows() {
-            if a.x == b.x {
-                for y in a.y.min(b.y)..=a.y.max(b.y) {
-                    blocked.insert((a.x, y).into());
-                }
-            } else {
-                for x in a.x.min(b.x)..=a.x.max(b.x) {
-                    blocked.insert((x, a.y).into());
+impl FromStr for Cave {
+    type Err = ParseIntError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut blocked = HashSet::<Coord>::new();
+        let mut max_y = 0;
+
+        for line in s.lines() {
+            let parts = line
+                .split(" -> ")
+                .map(Coord::from_str)
+                .collect::<Result<Vec<_>, _>>()?;
+
+            max_y = max_y.max(parts.iter().map(|c| c.y).max().unwrap());
+
+            for (a, b) in parts.iter().tuple_windows() {
+                if a.x == b.x {
+                    for y in a.y.min(b.y)..=a.y.max(b.y) {
+                        blocked.insert((a.x, y).into());
+                    }
+                } else {
+                    for x in a.x.min(b.x)..=a.x.max(b.x) {
+                        blocked.insert((x, a.y).into());
+                    }
                 }
             }
         }
-    }
 
-    let mut result = 0;
+        Ok(Cave {
+            blocked,
+            max_y,
+            sand_count: 0,
+        })
+    }
+}
+
+#[aoc(day14, part1)]
+pub fn part1(input: &str) -> Result<u32, ParseIntError> {
+    let mut cave = input.parse::<Cave>()?;
 
     loop {
         let mut sand = Coord { x: 500, y: 0 };
 
         loop {
-            if sand.y > max_y {
-                return Ok(result);
+            if sand.y > cave.max_y {
+                return Ok(cave.sand_count);
             }
 
-            if !blocked.contains(&sand.add(0, 1)) {
+            if cave.is_free(sand.add(0, 1)) {
                 sand = sand.add(0, 1);
                 continue;
             }
 
-            if !blocked.contains(&sand.add(-1, 1)) {
+            if cave.is_free(sand.add(-1, 1)) {
                 sand = sand.add(-1, 1);
                 continue;
             }
 
-            if !blocked.contains(&sand.add(1, 1)) {
+            if cave.is_free(sand.add(1, 1)) {
                 sand = sand.add(1, 1);
                 continue;
             }
 
-            blocked.insert(sand);
-            result += 1;
+            cave.insert_sand(sand);
+            break;
+        }
+    }
+}
+
+#[aoc(day14, part2)]
+pub fn part2(input: &str) -> Result<u32, ParseIntError> {
+    let mut cave = input.parse::<Cave>()?;
+
+    loop {
+        let mut sand = Coord { x: 500, y: 0 };
+
+        if !cave.is_free(sand) {
+            return Ok(cave.sand_count);
+        }
+
+        loop {
+            if sand.y > cave.max_y {
+                cave.insert_sand(sand);
+                break;
+            }
+
+            if cave.is_free(sand.add(0, 1)) {
+                sand = sand.add(0, 1);
+                continue;
+            }
+
+            if cave.is_free(sand.add(-1, 1)) {
+                sand = sand.add(-1, 1);
+                continue;
+            }
+
+            if cave.is_free(sand.add(1, 1)) {
+                sand = sand.add(1, 1);
+                continue;
+            }
+
+            cave.insert_sand(sand);
             break;
         }
     }
@@ -101,5 +167,11 @@ mod tests {
     fn test_case_1() {
         let result = super::part1("498,4 -> 498,6 -> 496,6\n503,4 -> 502,4 -> 502,9 -> 494,9");
         assert_eq!(result, Ok(24));
+    }
+
+    #[test]
+    fn test_case_2() {
+        let result = super::part2("498,4 -> 498,6 -> 496,6\n503,4 -> 502,4 -> 502,9 -> 494,9");
+        assert_eq!(result, Ok(93));
     }
 }
