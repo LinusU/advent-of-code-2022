e350e9af72cce41a52e66dcfeed1166b82501a02
Linus Unnebäck <linus@folkdatorn.se>
2022-12-15 10:55:32 +0100

    🎉 Add solution to Day 15 - Part 1

---
+++ src/day15.rs
@@ -0,0 +1,132 @@
+use std::{cmp::Ordering, num::ParseIntError, ops::RangeInclusive, str::FromStr};
+
+use aoc_runner_derive::aoc;
+use itertools::Itertools;
+
+#[cfg(test)]
+const TARGET_Y: i32 = 10;
+#[cfg(not(test))]
+const TARGET_Y: i32 = 2000000;
+
+#[derive(Debug)]
+struct Coord {
+    x: i32,
+    y: i32,
+}
+
+impl Coord {
+    fn distance_to(&self, other: &Coord) -> u32 {
+        self.x.abs_diff(other.x) + self.y.abs_diff(other.y)
+    }
+}
+
+impl FromStr for Coord {
+    type Err = ParseIntError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let (x, y) = s.split_once(',').unwrap();
+
+        Ok(Coord {
+            x: x.trim().strip_prefix("x=").unwrap().parse()?,
+            y: y.trim().strip_prefix("y=").unwrap().parse()?,
+        })
+    }
+}
+
+#[derive(Debug)]
+struct Sensor {
+    closest_beacon: Coord,
+    position: Coord,
+}
+
+impl FromStr for Sensor {
+    type Err = ParseIntError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let (sensor, beacon) = s.split_once(':').unwrap();
+
+        let sensor = sensor.trim().strip_prefix("Sensor at").unwrap();
+        let beacon = beacon.trim().strip_prefix("closest beacon is at").unwrap();
+
+        Ok(Sensor {
+            closest_beacon: beacon.parse()?,
+            position: sensor.parse()?,
+        })
+    }
+}
+
+#[aoc(day15, part1)]
+pub fn part1(input: &str) -> Result<usize, ParseIntError> {
+    let sensors = input
+        .lines()
+        .map(|line| line.parse::<Sensor>())
+        .collect::<Result<Vec<_>, _>>()?;
+
+    let mut ranges = sensors
+        .iter()
+        .filter_map(|sensor| {
+            let reach = sensor.position.distance_to(&sensor.closest_beacon);
+            let dy = sensor.position.y.abs_diff(TARGET_Y);
+
+            if dy > reach {
+                return None;
+            }
+
+            Some(RangeInclusive::new(
+                sensor.position.x.checked_sub_unsigned(reach - dy).unwrap(),
+                sensor.position.x.checked_add_unsigned(reach - dy).unwrap(),
+            ))
+        })
+        .collect::<Vec<_>>();
+
+    ranges.sort_by(|lhs, rhs| match lhs.start().cmp(rhs.start()) {
+        Ordering::Greater => Ordering::Greater,
+        Ordering::Less => Ordering::Less,
+        Ordering::Equal => lhs.end().cmp(rhs.end()),
+    });
+
+    let ranges = ranges.into_iter().fold(vec![], |mut mem, item| {
+        let Some(prev) = mem.pop() else {
+            mem.push(item);
+            return mem;
+        };
+
+        if *item.start() > (prev.end() + 1) {
+            mem.push(prev);
+            mem.push(item);
+            return mem;
+        }
+
+        mem.push(RangeInclusive::new(
+            *prev.start(),
+            *item.end().max(prev.end()),
+        ));
+
+        mem
+    });
+
+    let beacons_in_target = sensors
+        .iter()
+        .filter(|sensor| sensor.closest_beacon.y == TARGET_Y)
+        .map(|sensor| &sensor.closest_beacon)
+        .filter(|beacon| ranges.iter().any(|range| range.contains(&beacon.x)))
+        .map(|beacon| beacon.x)
+        .unique()
+        .count();
+
+    let scanned_positions = ranges
+        .iter()
+        .map(|range| (range.end() - range.start() + 1) as usize)
+        .sum::<usize>();
+
+    Ok(scanned_positions - beacons_in_target)
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_case_1() {
+        let result = super::part1("Sensor at x=2, y=18: closest beacon is at x=-2, y=15\nSensor at x=9, y=16: closest beacon is at x=10, y=16\nSensor at x=13, y=2: closest beacon is at x=15, y=3\nSensor at x=12, y=14: closest beacon is at x=10, y=16\nSensor at x=10, y=20: closest beacon is at x=10, y=16\nSensor at x=14, y=17: closest beacon is at x=10, y=16\nSensor at x=8, y=7: closest beacon is at x=2, y=10\nSensor at x=2, y=0: closest beacon is at x=2, y=10\nSensor at x=0, y=11: closest beacon is at x=2, y=10\nSensor at x=20, y=14: closest beacon is at x=25, y=17\nSensor at x=17, y=20: closest beacon is at x=21, y=22\nSensor at x=16, y=7: closest beacon is at x=15, y=3\nSensor at x=14, y=3: closest beacon is at x=15, y=3\nSensor at x=20, y=1: closest beacon is at x=15, y=3");
+        assert_eq!(result, Ok(26));
+    }
+}
