adaa4943084b6ebb1b18a6caec2466ec27443d1c
Linus Unnebäck <linus@folkdatorn.se>
2022-12-18 17:37:18 +0100

    🎉 Add solution to Day 18 - Part 2

--- src/day18.rs
+++ src/day18.rs
@@ -6,6 +6,10 @@ use aoc_runner_derive::aoc;
 struct Pos(u16);
 
 impl Pos {
+    fn new(x: u16, y: u16, z: u16) -> Pos {
+        Pos(x | y << 5 | z << 10)
+    }
+
     fn neighbours(&self) -> Vec<Pos> {
         vec![
             Pos(self.0 - 1),
@@ -16,6 +20,15 @@ impl Pos {
             Pos(self.0 + (1 << 10)),
         ]
     }
+
+    fn is_edge(&self) -> bool {
+        (self.0 & 0b000000000011111) == 0
+            || (self.0 & 0b000000000011111) == 31
+            || (self.0 & 0b000001111100000) >> 5 == 0
+            || (self.0 & 0b000001111100000) >> 5 == 31
+            || (self.0 & 0b111110000000000) >> 10 == 0
+            || (self.0 & 0b111110000000000) >> 10 == 31
+    }
 }
 
 impl FromStr for Pos {
@@ -24,10 +37,12 @@ impl FromStr for Pos {
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         let mut parts = s.split(',').map(|s| s.parse::<u16>());
 
-        // Add one to avoid having to deal with subtracting 1 from 0 in the neighbours function
-        Ok(Pos((parts.next().unwrap()? + 1)
-            | (parts.next().unwrap()? + 1) << 5
-            | (parts.next().unwrap()? + 1) << 10))
+        // Offset a bit to avoid dealing with 0/1 values in rest of code
+        Ok(Pos::new(
+            parts.next().unwrap()? + 3,
+            parts.next().unwrap()? + 3,
+            parts.next().unwrap()? + 3,
+        ))
     }
 }
 
@@ -53,6 +68,50 @@ pub fn part1(input: &str) -> Result<usize, ParseIntError> {
     Ok(result)
 }
 
+#[aoc(day18, part2)]
+pub fn part2(input: &str) -> Result<usize, ParseIntError> {
+    let cubes = input
+        .lines()
+        .map(|s| s.parse::<Pos>())
+        .collect::<Result<BTreeSet<_>, _>>()?;
+
+    let mut outside = BTreeSet::<Pos>::new();
+    let mut queue = Vec::<Pos>::new();
+
+    outside.insert(Pos::new(1, 1, 1));
+    queue.push(Pos::new(1, 1, 1));
+
+    while let Some(pos) = queue.pop() {
+        for neighbour in pos.neighbours() {
+            if outside.contains(&neighbour) {
+                continue;
+            }
+
+            if cubes.contains(&neighbour) {
+                continue;
+            }
+
+            outside.insert(neighbour);
+
+            if !neighbour.is_edge() {
+                queue.push(neighbour);
+            }
+        }
+    }
+
+    let mut result = 0;
+
+    for cube in cubes.iter() {
+        for neighbour in cube.neighbours() {
+            if outside.contains(&neighbour) {
+                result += 1;
+            }
+        }
+    }
+
+    Ok(result)
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
@@ -71,5 +130,23 @@ mod tests {
     fn test_case_3() {
         let result = super::part1("2,2,2\n1,2,2\n3,2,2\n2,1,2\n2,3,2\n2,2,1\n2,2,3\n2,2,4\n2,2,6\n1,2,5\n3,2,5\n2,1,5\n2,3,5\n");
         assert_eq!(result, Ok(64));
+    }
+
+    #[test]
+    fn test_case_4() {
+        let result = super::part2("1,1,1");
+        assert_eq!(result, Ok(6));
+    }
+
+    #[test]
+    fn test_case_5() {
+        let result = super::part2("1,1,1\n2,1,1");
+        assert_eq!(result, Ok(10));
+    }
+
+    #[test]
+    fn test_case_6() {
+        let result = super::part2("2,2,2\n1,2,2\n3,2,2\n2,1,2\n2,3,2\n2,2,1\n2,2,3\n2,2,4\n2,2,6\n1,2,5\n3,2,5\n2,1,5\n2,3,5\n");
+        assert_eq!(result, Ok(58));
     }
 }
